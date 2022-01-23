8b6561dac715265136e9a5faebe8df57a6650df6
Boas-World <84286574+Boas-World@users.noreply.github.com>
2022-01-23 15:55:36 +0100

    Update new functions on tk_short
    Add Cube,Rect,Oval and Circle function

--- py_shortcut.py
+++ py_shortcut.py
@@ -4,5 +4,13 @@ class tk_short:
       from tkinter import*
     except:
       pass
-    print("Tk short version: 1.0.0")
+    print("Tk short version: 1.0.2")
+  def Cube (C,x,y,L):
+    C.create_rectangle(x,y,x+L,y+L)
+  def Rect (C,x,y,L,l):
+    C.create_rectangle(x,y,x+L,y+l)
+  def Circle (C,x,y,R):
+    C.create_oval(x,y,x+R,y+R)
+  def Oval (C,x,y,L,l):
+    C.create_oval(x,y,x+L,y+l)
   
