5e722b09c979f6760b5f01ca9142f7559c19ccf7
Boas-World <84286574+Boas-World@users.noreply.github.com>
2022-01-27 13:05:14 +0100

    Added turtle_short
    Added foright, forleft, backright, backleft function

--- py_shortcut.py
+++ py_shortcut.py
@@ -13,4 +13,30 @@ class tk_short:
     C.create_oval(x,y,x+R,y+R)
   def Oval (C,x,y,L,l):
     C.create_oval(x,y,x+L,y+l)
-  
+
+    
+    
+class Turtle_short:
+  def __init__(self):
+    try:
+      from turtle import*
+     except:
+      pass
+   def forigth(slef,A,a):
+    for i in range(0,A):
+      forward(1)
+      right(a/A)
+   def forleft(slef,A,a):
+    for i in range(0,A):
+      forward(1)
+      left(a/A)
+   def backrigth(slef,A,a):
+    for i in range(0,A):
+      backward(1)
+      right(a/A)
+   def backleft(slef,A,a):
+    for i in range(0,A):
+      backward(1)
+      left(a/A)
+      
+      
